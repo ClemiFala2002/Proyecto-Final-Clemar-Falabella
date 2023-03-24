7f1fdb95db958360c1a0a593e2188e0986c48972
ClemiFala2002 <clemifala13@gmail.com>
2023-03-24 03:46:04 -0300

    v1.3

--- OnlyTrucks/views.py
+++ OnlyTrucks/views.py
@@ -7,85 +7,6 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 def index(request):
     return render(request, "OnlyTrucks/index.html")
 
-def Resultado_de_Camion(request):
-    
-    Camiones = Camion.objects.all()
-    return render(request, "OnlyTrucks/Resultado_de_Busqueda.html",{"Camiones": Camiones})
-
-def mostrar_Camion(request):
-    
-    context = {
-        "form": CamionForm(),
-        "Camiones": Camion.objects.all(),
-    }
-    
-    return render(request, "OnlyTrucks/Camiones.html", context )
-
-def agregar_Camion(request):
-    post_form = CamionForm(request.POST)
-    post_form.save()
-    context = {
-        "form": CamionForm(),
-        "Camiones": Camion.objects.all(),
-    }
-    
-    return render(request, "OnlyTrucks/Camiones.html", context)
-
-def buscar_Camion(request):
-    criterio= request.GET.get("criterio")
-    context = {
-        "Camiones":Camion.objects.filter(Modelo_de_su_Camion__icontains=criterio).all(),
-    }
-    
-    return render(request, "OnlyTrucks/Resultado_de_Busqueda.html", context)
-
-
-def Resultado_de_Remolque(request):
-    
-    remolques = Remolques.objects.all()
-    return render(request, "OnlyTrucks/Resultado_de_Busqueda_R.html",{"Remolques": remolques})
-
-def mostrar_Remolque(request):
-    
-    context = {
-        "formr": RemolquesForm(),
-        "Remolques": Remolques.objects.all(),
-    }
-    
-    return render(request, "OnlyTrucks/Remolques.html", context )
-
-def agregar_Remolque(request):
-    remolques_form = RemolquesForm(request.POST)
-    remolques_form.save()
-    context = {
-        "formr": RemolquesForm(),
-        "Remolques": Remolques.objects.all(),
-    }
-    return render(request, "OnlyTrucks/Remolques.html", context)
-
-def Resultado_de_Consesionaria(request):
-    
-    concesionaria = Concesionaria.objects.all()
-    return render(request, "OnlyTrucks/Resultado_de_Busqueda_C.html",{"concesionaria": concesionaria})
-
-def mostrar_Consesionaria(request):
-    
-    context = {
-        "formc": ConsesionariaForm(),
-        "Consesionaria": Concesionaria.objects.all(),
-    }
-    
-    return render(request, "OnlyTrucks/Consesionaria.html", context )
-
-def agregar_Consesionaria(request):
-    remolques_form = ConsesionariaForm(request.POST)
-    remolques_form.save()
-    context = {
-        "formc": ConsesionariaForm(),
-        "Consesionaria": Concesionaria.objects.all(),
-    }
-    return render(request, "OnlyTrucks/Consesionaria.html", context)
-
 class CamionList(ListView):
     model = Camion
     context_object_name = "Camiones"
@@ -114,5 +35,27 @@ class CamionSearch(ListView):
     def get_queryset(self):
         criterio = self.request.GET.get("criterio")
         result= Camion.objects.filter(Modelo_de_su_Camion__icontains=criterio).all()
-        return result   
+        return result 
+
     
+class RemolqueList(ListView):
+    model = Remolques
+    context_object_name = "Remolques"  
+    
+class RemolqueDetail(DetailView):
+    model = Remolques 
+    context_object_name = "Remolque"      
+    
+class RemolqueUpdate(UpdateView):
+    model = Remolques
+    success_url = reverse_lazy("Remolques-list")
+    fields = '__all__' 
+    
+class RemolqueDelete(DeleteView):
+    model = Remolques
+    success_url = reverse_lazy("Remolques-list")
+    
+class RemolqueCreate(CreateView):
+    model = Remolques    
+    success_url = reverse_lazy("Remolques-list") 
+    fields = '__all__'    
--- Tercera_Preentrega/urls.py
+++ Tercera_Preentrega/urls.py
@@ -15,25 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from OnlyTrucks.views import index, mostrar_Camion, agregar_Camion, buscar_Camion, Resultado_de_Camion, mostrar_Remolque, agregar_Remolque, mostrar_Consesionaria, agregar_Consesionaria, Resultado_de_Remolque,Resultado_de_Consesionaria, CamionList, CamionDetail, CamionUpdate, CamionDelete, CamionCreate, CamionSearch
+from OnlyTrucks.views import index, CamionList, CamionDetail, CamionUpdate, CamionDelete, CamionCreate, CamionSearch, RemolqueList, RemolqueDetail, RemolqueUpdate, RemolqueDelete, RemolqueCreate
 
 urlpatterns = [
    path('admin/', admin.site.urls),
    path('', index, name="index"), 
-   path('Resultado de Busqueda/', Resultado_de_Camion, name="Resultado Camion"), 
-   path('Camiones/', mostrar_Camion, name="Camion"), 
-   path('Camiones/agregar', agregar_Camion, name="agregar-camion"),
-   path('Camiones/buscar', buscar_Camion, name="buscar-camion"),
-   path('Acoplados/', mostrar_Remolque, name="Remolque"),
-   path('Acoplados/agregar', agregar_Remolque, name="Agregar-Remolque"),
-   path('Concesionario/', mostrar_Consesionaria, name="Consesionaria1"),
-   path('Concesionario/agregar', agregar_Consesionaria, name="Agregar-Consesionaria"),
-   path('Resultado de BusquedaR/', Resultado_de_Remolque, name="Resultado BusquedaR"), 
-   path('Resultado de BusquedaC/', Resultado_de_Consesionaria, name="Resultado BusquedaC"),
    path('Camion/list', CamionList.as_view() , name="Camion-list"),
    path('Camion/<pk>/detail', CamionDetail.as_view() , name="Camion-detail"),
    path('Camion/<pk>/update', CamionUpdate.as_view() , name="Camion-update"),
    path('Camion/<pk>/delete', CamionDelete.as_view() , name="Camion-delete"),
    path('Camion/create', CamionCreate.as_view() , name="Camion-create"),
    path('Camion/search', CamionSearch.as_view() , name="Camion-search"),
+   path('Acoplados/list', RemolqueList.as_view() , name="Remolques-list"),
+   path('Acoplados/<pk>/detail', RemolqueDetail.as_view() , name="Remolques-detail"),
+   path('Acoplados/<pk>/update', RemolqueUpdate.as_view() , name="Remolques-update"),
+   path('Acoplados/<pk>/delete', RemolqueDelete.as_view() , name="Remolques-delete"),
+   path('Acoplados/create', RemolqueCreate.as_view() , name="Remolques-create"),
 ]
